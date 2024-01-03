d81b3a219cf450eda22985797767ca7359ebb6be
Guilherme <alonsogui011@gmail.com>
2024-01-03 20:02:24 -0300

    Básico de ReactJS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import MyBody from './MyBody';
 
 function App() {
+
+  const [str, setStr] = useState('Olá mundo');
+
+  function handleClick(){
+    setStr('Olá DankiCode');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{str}</h2>
+      <button onClick={()=>handleClick()}>Clique aqui</button>
+      <MyBody></MyBody>
     </div>
   );
 }
---
+++ src/MyBody.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MyBody = () => {
+    return(
+        <h2>Meu componente body!</h2>
+    );
+}
+
+export default MyBody;
